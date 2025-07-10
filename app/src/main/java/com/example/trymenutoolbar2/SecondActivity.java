15e749310ddcaed1bdaa825531c436dfe4177ee7
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-07-10 14:21:53 +0200

    l

--- app/src/main/java/com/example/trymenutoolbar2/SecondActivity.java
+++ app/src/main/java/com/example/trymenutoolbar2/SecondActivity.java
@@ -1,7 +1,29 @@
 package com.example.trymenutoolbar2;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+    private Button button;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity_main);
+
+        button = findViewById(R.id.button2);
+    }
+
+    private void clickToBack(int id) {
+        button = findViewById(id);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
 
 }
