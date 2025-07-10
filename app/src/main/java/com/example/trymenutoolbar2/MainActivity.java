40abb3a3907bd592d6811e7536e4d9fc1788dcdc
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-07-10 14:45:30 +0200

    l

--- app/src/main/java/com/example/trymenutoolbar2/MainActivity.java
+++ app/src/main/java/com/example/trymenutoolbar2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.trymenutoolbar2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/trymenutoolbar2/SecondActivity.java
+++ app/src/main/java/com/example/trymenutoolbar2/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.example.trymenutoolbar2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,7 +22,8 @@ public class SecondActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
     }
